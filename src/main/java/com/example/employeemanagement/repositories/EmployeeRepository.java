0fbc17fe225440666c0ce7a2c1df3407c84e0364
manthan <manthanchauhan@indiagold.co>
2023-01-21 10:01:29 +0530

    list all employees api done

---
+++ src/main/java/com/example/employeemanagement/repositories/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.example.employeemanagement.repositories;
+
+import com.example.employeemanagement.models.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+}
